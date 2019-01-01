951c5d527ccb84ab886554b0ba028bf85eae0d2d
Mike Liu <mikeliu8492@gmail.com>
2019-01-01 14:50:17 -0600

    added about page

--- routes/news.js
+++ routes/news.js
@@ -13,33 +13,47 @@ router.get("/", (req,res,next) => {
 })
 
 router.get("/top_headlines", async (req,res) => {
-    
-    const response = await newsapi.v2.topHeadlines({
-        sources: 'medical-news-today',
-        language: 'en',
-        pageSize: 7,
-        page: 1
-    });
+    try {
+        const response = await newsapi.v2.topHeadlines({
+            sources: 'medical-news-today',
+            language: 'en',
+            pageSize: 7,
+            page: 1
+        });
+        return res.status(200).json(response);
+    }
+    catch(err) {
+        return res.status(500).json({message: err.toString(), errors: {apiCall: err.toString()}});
+    }
 
-    res.status(200).json(response);
+
+    
 })
 
-router.get("/personal_top_headlines", async(req,res) => {
-    const topics = ['psychology', 'psychiatry', 'mental health'];
-    if(!topics) {
-        return res.status(400).json({message: "You're supposed to send a list of terms"});
-    }
+router.post("/personal_top_headlines", async(req,res) => {
     
-    const questionPayload = topics.join(" OR ");
+    try {
+        const topics = req.body.topics //['cardiology', 'heart', 'cardiac', 'cardiovascular'];
+        if(!topics || topics.length <= 0) {
+            return res.status(400).json({message: "You're supposed to send a list of terms",  errors: {apiCall: "You're supposed to send a list of terms"}});
+        }
+        
+        const questionPayload = topics.join(" OR ");
+    
+        const response = await newsapi.v2.everything({
+            sources: 'medical-news-today',
+            q: questionPayload,
+            from: "2018-12-25",
+            sortBy: "relevancy",
+            pageSize: 10,
+            page:1
+        });
 
-    const response = await newsapi.v2.everything({
-        sources: 'medical-news-today',
-        q: questionPayload,
-        sortBy: "relevancy",
-        pageSize: 5,
-        page:1
-    });
-    res.status(200).json(response);
+        return res.status(200).json(response);
+    }
+    catch(err) {
+        return res.status(500).json({message: "You're supposed to send a list of terms",  errors: {apiCall: "You're supposed to send a list of terms"}});
+    }
 })
 
 module.exports = router;
