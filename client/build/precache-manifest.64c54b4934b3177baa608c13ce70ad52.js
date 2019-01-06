a5c1ac6e14add0caae1de1b0c3a66c72e45f7a7c
Mike Liu <mikeliu8492@gmail.com>
2019-01-05 23:56:02 -0600

    first production commit

---
+++ client/build/precache-manifest.64c54b4934b3177baa608c13ce70ad52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb365336768436f37b7ec8332a0aa586",
+    "url": "/static/media/mnt-logo.fb365336.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "66181526b63e508e6343",
+    "url": "/static/js/main.66181526.chunk.js"
+  },
+  {
+    "revision": "9266429a258c565967f0",
+    "url": "/static/js/1.9266429a.chunk.js"
+  },
+  {
+    "revision": "66181526b63e508e6343",
+    "url": "/static/css/main.41ffbeaa.chunk.css"
+  },
+  {
+    "revision": "24b7bd1aa633a697e0405afae3b4f157",
+    "url": "/index.html"
+  }
+];
